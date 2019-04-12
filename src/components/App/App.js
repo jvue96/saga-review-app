5ebf660141dab7d46484707af6348714caba4b70
Juno Vue <Juno.Vue@Hotmail.com>
2019-04-12 10:50:37 -0500

    imported {connect} from react redux in app.js to text a dispatch for 'SET_CHARACTERS'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// have to import connect to talk to redux 
+import { connect } from 'react-redux'; 
+
 class App extends Component {
+
+  componentDidMount () {
+    this.props.dispatch( {type: 'SET_CHARACTERS', payload: [`hello, testing`] })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,4 +21,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// set up for redux & rediux saga 
+import { createStore, combineReducers, applyMiddleware } from 'redux'; 
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'; 
+
+
+// character list reducer - will hold characters from server 
+// action SET_CHARACTERS sent by saga with payload
+const characterListReducer = (state = [], action) => {
+    if(action.type === 'SET_CHARACTERS') {
+        return action.payload; 
+    }
+        return state; 
+} // end characterListReducer
+
+const reduxStore = createStore(
+    combineReducers({
+        characterListReducer,
+    }),
+    applyMiddleware(logger)
+)
+
+ReactDOM.render(
+<Provider store={reduxStore}>
+    <App />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
